e014d7f1970f53a01a21cbb6a8f27da7089d8ea5
Paul Lessing <paul@paullessing.com>
2017-05-02 23:01:19 +0100

    Move image logic out to ImageRouter

--- src/routers/api.router.ts
+++ src/routers/api.router.ts
@@ -1,99 +1,17 @@
 import { Service } from '../util/inject';
-import { Get, Post, Response } from 'express-router-decorators';
-import * as express from 'express';
-import * as Busboy from 'busboy';
-import { FileService } from '../files/file.service';
-import { UploadedFile } from '../files/uploaded-file.model';
-import { ImageSizes } from '../interfaces/image-sizes';
+import { Use, UseType } from 'express-router-decorators';
+import { ImageRouter } from './image.router';
 
 @Service()
 export class ApiRouter {
 
+  @Use('/', UseType.ROUTER)
+  public imageRouter: ImageRouter;
+
   constructor(
-    private fileService: FileService
-  ) {}
-
-  @Get('/')
-  public getRoot(): Promise<Response> {
-    return Response.resolve('Hello API!');
+    imageRouter: ImageRouter
+  ) {
+    this.imageRouter = imageRouter;
   }
 
-  @Get('/image')
-  public getImageUploadForm(): Promise<Response> {
-    // TEMPORARY CODE
-    return Response.resolve(`
-<html><head></head><body>
-  <form method="POST" enctype="multipart/form-data">
-    <input type="file" name="image"><br />
-    <input type="submit">
-  </form>
-</body></html>`);
-  }
-
-  @Post('/image')
-  public uploadImage(req: express.Request, res: express.Response): Promise<UploadedFile> {
-    const busboy = new Busboy({
-      headers: req.headers,
-      limits: {
-        fileSize: 1000000
-      }
-    });
-
-    let uploadedFile: Promise<UploadedFile>;
-
-    return new Promise((resolve, reject) => {
-      busboy.on('file', (
-        fieldname: string,
-        file: NodeJS.ReadableStream,
-        filename: string,
-        encoding: string,
-        mimetype: string
-      ) => {
-        if (fieldname === 'image') {
-          uploadedFile = this.fileService.uploadFile(file)
-            .then((file: UploadedFile) => {
-              return file;
-            });
-        } else {
-          // Skip
-          file.resume();
-        }
-      });
-      busboy.on('finish', function() {
-        if (uploadedFile) {
-          uploadedFile.then((file: UploadedFile) => Response.success(201, file))
-            .then(resolve, reject);
-        } else {
-          reject(Response.error(400, 'Missing "image" field in request'));
-        }
-      });
-      busboy.on('error', (err: any) => {
-        console.error(err);
-        reject(Response.error(500));
-      });
-
-      req.pipe(busboy);
-    });
-  }
-
-  @Get('/image/:imageId/:size')
-  public getOriginalImage(req: express.Request, res: express.Response): void {
-    const imageId = req.params['imageId'];
-    const size = req.params['size'];
-    console.log('image id', imageId, size);
-
-    if (ImageSizes.values().indexOf(size) < 0) {
-      return res.sendStatus(404).end();
-    }
-
-    this.fileService.getFile(imageId, size)
-      .then((data: NodeJS.ReadableStream) => {
-        data.pipe(res);
-      }, () => {
-        res.sendStatus(404).end();
-      }).catch((e) => {
-        console.error(e);
-        res.sendStatus(500).end();
-    });
-  }
 }
---
+++ src/routers/image.router.ts
@@ -0,0 +1,95 @@
+import { Service } from '../util/inject';
+import { Get, Post, Response } from 'express-router-decorators';
+import * as express from 'express';
+import * as Busboy from 'busboy';
+import { FileService } from '../files/file.service';
+import { UploadedFile } from '../files/uploaded-file.model';
+import { ImageSizes } from '../interfaces/image-sizes';
+
+@Service()
+export class ImageRouter {
+
+  constructor(
+    private fileService: FileService
+  ) {}
+
+  @Get('/')
+  public getImageUploadForm(): Promise<Response> {
+    // TEMPORARY CODE
+    return Response.resolve(`
+<html><head></head><body>
+  <form method="POST" enctype="multipart/form-data">
+    <input type="file" name="image"><br />
+    <input type="submit">
+  </form>
+</body></html>`);
+  }
+
+  @Post('/')
+  public uploadImage(req: express.Request, res: express.Response): Promise<UploadedFile> {
+    const busboy = new Busboy({
+      headers: req.headers,
+      limits: {
+        fileSize: 1000000
+      }
+    });
+
+    let uploadedFile: Promise<UploadedFile>;
+
+    return new Promise((resolve, reject) => {
+      busboy.on('file', (
+        fieldname: string,
+        file: NodeJS.ReadableStream,
+        filename: string,
+        encoding: string,
+        mimetype: string
+      ) => {
+        if (fieldname === 'image') {
+          uploadedFile = this.fileService.uploadFile(file)
+            .then((file: UploadedFile) => {
+              return file;
+            });
+        } else {
+          // Skip
+          file.resume();
+        }
+      });
+      busboy.on('finish', function() {
+        if (uploadedFile) {
+          uploadedFile.then((file: UploadedFile) => Response.success(201, file))
+            .then(resolve, reject);
+        } else {
+          reject(Response.error(400, 'Missing "image" field in request'));
+        }
+      });
+      busboy.on('error', (err: any) => {
+        console.error(err);
+        reject(Response.error(500));
+      });
+
+      req.pipe(busboy);
+    });
+  }
+
+  @Get('/:imageId')
+  @Get('/:imageId/:size')
+  public getOriginalImage(req: express.Request, res: express.Response): void {
+    const imageId = req.params['imageId'];
+    const size = req.params['size'] || ImageSizes.ORIGINAL;
+    console.log('image id', imageId, size);
+
+    if (ImageSizes.values().indexOf(size) < 0) {
+      return res.sendStatus(404).end();
+    }
+
+    this.fileService.getFile(imageId, size)
+      .then((data: NodeJS.ReadableStream) => {
+        data.pipe(res);
+      }, () => {
+        res.sendStatus(404).end();
+      }).catch((e) => {
+      console.error(e);
+      res.sendStatus(500).end();
+    });
+  }
+}
