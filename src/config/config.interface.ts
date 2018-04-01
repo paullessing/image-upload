817b38f9fb7897feac82ad3536b3a43e9c78b24b
paul.lessing <paul@paullessing.com>
2018-04-01 14:06:41 +0100

    Remove redundant methods

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -13,39 +13,24 @@ export interface DiskStorageConfig {
   type: 'disk';
   dir: string;
 }
-export function isDiskStorage(config: any): config is DiskStorageConfig {
-  return config && config.hasOwnProperty('type') && config['type'] === 'disk';
-}
 
 export interface AwsStorageConfig {
   type: 'aws';
   path: string;
 }
-export function isAwsStorage(config: any): config is AwsStorageConfig {
-  return config && config.hasOwnProperty('type') && config['type'] === 'aws';
-}
 
 export interface MemoryDatabaseConfig {
   type: 'memory';
-}
-export function isMemoryDatabase(config: any): config is MemoryDatabaseConfig {
-  return config && config.hasOwnProperty('type') && config['type'] === 'memory';
 }
 
 export interface FileDatabaseConfig {
   type: 'file';
   path: string;
 }
-export function isFileDatabase(config: any): config is FileDatabaseConfig {
-  return config && config.hasOwnProperty('type') && config['type'] === 'file';
-}
 
 export interface MongoDbConfig {
   type: 'mongodb';
   url: string;
-}
-export function isMongoDb(config: any): config is MongoDbConfig {
-  return config && config.hasOwnProperty('type') && config['type'] === 'mongodb';
 }
 
 export interface ImageSizeConfig {
