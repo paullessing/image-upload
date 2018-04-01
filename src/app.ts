32c48d90a82f730d1af5910049f16c8e4726e8ce
paul.lessing <paul@paullessing.com>
2018-04-01 17:58:38 +0100

    Read config from input file

--- src/app.ts
+++ src/app.ts
@@ -2,41 +2,19 @@ import * as express from 'express';
 import { createRouter } from 'express-router-decorators';
 import * as morgan from 'morgan';
 import * as winston from 'winston';
-import * as fs from 'fs';
-import { config } from './config/config';
 
 import { Dependencies } from './util/inject';
 import { ApiRouter } from './routers/api.router';
+import { CONFIG, Config } from './config';
 
-const hasEnvConfigPath = !!process.env.CONFIG_PATH;
-
-// TODO load config from /var/config.json if no env variable is set and that file exists
-if (hasEnvConfigPath) {
-  try {
-    const configPath = process.env.CONFIG_PATH;
-    if (!fs.existsSync(configPath)) {
-      console.error(`File does not exist: ${configPath}`);
-      process.exit(1);
-    }
-    const extraConfig = fs.readFileSync(configPath).toString();
-    if (!extraConfig.length) {
-      console.error(`File is empty: ${configPath}`);
-      process.exit(1);
-    }
-
-    Object.assign(config, JSON.parse(extraConfig));
-  } catch (e) {
-    console.error('Failed to load config', e);
-    process.exit(1);
-  }
-}
+const container = Dependencies.createContainer(__dirname);
+const config = container.get<Config>(CONFIG);
 
 const logLevel = config.logLevel || 'debug';
 Object.assign(winston, { level: logLevel }); // Workaround because the typings file says this is readonly (but it isn't)
 
 const app = express();
 
-const container = Dependencies.createContainer(__dirname);
 const router = createRouter(container.get(ApiRouter), { log: winston });
 
 app.use((morgan as any as morgan.Morgan)('dev'));
--- src/config/config.bindings.ts
+++ src/config/config.bindings.ts
@@ -1,11 +1,40 @@
 import { Bindings } from '../util/inject';
 import { interfaces } from 'inversify';
 import { config } from './config';
-import { CONFIG } from './config.interface';
+import { Config, CONFIG } from './config.interface';
+import * as fs from 'fs';
 
 @Bindings()
 export class ConfigBindings {
   public static $bind(bind: interfaces.Bind): void {
-    bind(CONFIG).toConstantValue(config); // TODO read from config file
+    const configFile = readFile(process.env.CONFIG_FILE);
+
+    if (configFile) {
+      bind(CONFIG).toConstantValue(configFile);
+      return;
+    }
+
+    const fallbackFile = readFile('/var/config.json');
+    if (fallbackFile) {
+      bind(CONFIG).toConstantValue(fallbackFile);
+      return;
+    }
+
+    console.info('No config file found, falling back to defaults');
+    bind(CONFIG).toConstantValue(config);
   }
+}
+
+function readFile(location: string): Config | null {
+  if (!location) {
+    return null;
+  }
+  if (!fs.existsSync(location) || !fs.statSync(location).isFile()) {
+    return null;
+  }
+  const raw = fs.readFileSync(location).toString();
+  if (!raw.trim().length) {
+    return null;
+  }
+  return JSON.parse(raw);
 }
